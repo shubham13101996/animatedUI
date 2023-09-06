c1a9b01d2a4cd1cfb910a82ca7a0409077e73d61
shubhampathak <shubham.pathak926@gmail.com>
2023-09-06 13:28:56 +0530

    built out/ meet teh ahed app section animated ui

--- constants/index.js
+++ constants/index.js
@@ -27,9 +27,9 @@ export const exploreWorlds = [
 ];
 
 export const startingFeatures = [
+  "A personalised pocket coach that provides bite-sized, science-driven tools to boost emotional intelligence",
+  " Think of it as a pocket cheerleader towards a better, more fulfilling",
   "Find a world that suits you and you want to enter",
-  "Enter the world by reading basmalah to be safe",
-  "No need to beat around the bush, just stay on the gas and have fun",
 ];
 
 export const newFeatures = [
